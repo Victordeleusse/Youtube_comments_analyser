f41be8f77add1bf418b0ad1043ceac9295be41eb
victor <victordeleusse@gmail.com>
2024-04-15 13:45:11 +0200

    db local implementation

--- run_ollama_analysis.py
+++ run_ollama_analysis.py
@@ -31,76 +31,46 @@ def check_if_model_is_available(model_name: str) -> None:
         ollama.pull(model_name, stream=True)
     else:
         print(f"The model {model_name} is already present locally.")
-# def comment_content(row_string: str):
-#     prompt = "As a youtube and langage expert, analyse this sentence where all comments for the video are separated by a " | " and provide a global sentiment for all these comments." + row_string
 
-#     stream = ollama.chat(
-#         model='llama2',
-#         messages=[{'role': 'user', 'content': prompt}],
-#         stream=True
-#     )
-#     for chunk in stream:
-#         print(chunk['message']['content'], end='', flush=True)
 
-# def get_comments_to_row_string(bucket_name, video_ids: list):
-def test():
-    # available = __is_model_available_locally('llama2')
-    # if not available:
-    #     ollama.pull('llama2')
-    check_if_model_is_available('llama2:latest')
-    response = ollama.chat(model='llama2', messages=[
-    {'role': 'user',
-    'content': 'Why is the sky blue?'},
-    ])
-    print(response['message']['content'])
-    # try:
-    #     check_if_model_is_available('llama2')
-    #     storage_client = storage.Client()
-    #     bucket = storage_client.bucket(bucket_name)
-    #     for video in video_ids:
-    #         video_name = get_video_info(video)[0]
-    #         destination_blob_name = f"{video_name}.json".replace(" ", "_").lower()
-    #         blob = bucket.blob(destination_blob_name)
-    #         if blob.exists():
-    #             existing_data = blob.download_as_text()
-    #             if existing_data:
-    #                 existing_data_json = json.loads(existing_data) if existing_data else []
-    #                 comments_one_str = ''
-    #                 for message in existing_data_json:
-    #                     comments_one_str += message["text"] + " | "
-    #                 comment_content(comments_one_str)    
-    # except Exception as e:
-    #     print(f"An error occurred! {e}")
+def comment_content(row_string: str):
+    prompt = "As a youtube and langage expert, analyse this sentence where all comments for the video are separated by a " | " and please, send me the number of message you can count, and then provide a global sentiment for all these comments." + row_string
 
-# default_args = {
-#     "owner": "airflow",
-#     "depends_on_past": False,
-#     "start_date": datetime(2024, 4, 11),
-#     "email_on_failure": False,
-#     "email_on_retry": False,
-#     "retries": 1,
-#     "retry_delay": timedelta(minutes=5),
-# }
+    stream = ollama.chat(
+        model='llama2',
+        messages=[{'role': 'user', 'content': prompt}],
+        stream=True
+    )
+    for chunk in stream:
+        print(chunk['message']['content'], end='', flush=True)
 
-# dag = DAG(
-#     'youtube_comment_analysis',
-#     default_args=default_args,
-#     description='Analyze YouTube comments using Ollama',
-#     schedule_interval='@daily',
-#     catchup=False,
-# )
-
-# analyze_comments = PythonOperator(
-#     task_id='analyze_youtube_comments',
-#     python_callable=get_comments_to_row_string,
-#     op_kwargs={
-#         "bucket_name": os.getenv("TF_VAR_NAME"),
-#         "video_ids": [os.getenv("VIDEO_ID_1")],
-#         "api_key": os.getenv("KEY_API"),
-#     },
-#     # provide_context=True,
-#     dag=dag,
-# )
+# def test():
+    # check_if_model_is_available('llama2:latest')
+    # response = ollama.chat(model='llama2', messages=[
+    # {'role': 'user',
+    # 'content': 'Why is the sky blue?'},
+    # ])
+    # print(response['message']['content'])
+    
+def get_comments_to_row_string(bucket_name, video_ids: list):
+    try:
+        check_if_model_is_available('llama2:latest')
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        for video in video_ids:
+            video_name = get_video_info(video)[0]
+            destination_blob_name = f"{video_name}.json".replace(" ", "_").lower()
+            blob = bucket.blob(destination_blob_name)
+            if blob.exists():
+                existing_data = blob.download_as_text()
+                if existing_data:
+                    existing_data_json = json.loads(existing_data) if existing_data else []
+                    comments_one_str = ''
+                    for message in existing_data_json:
+                        comments_one_str += message["text"] + " | "
+                    comment_content(comments_one_str)    
+    except Exception as e:
+        print(f"An error occurred! {e}")
 
 default_args = {
     "owner": "airflow",
@@ -122,10 +92,40 @@ dag = DAG(
 
 analyze_comments = PythonOperator(
     task_id='analyze_youtube_comments',
-    python_callable=test,
+    python_callable=get_comments_to_row_string,
+    op_kwargs={
+        "bucket_name": os.getenv("TF_VAR_NAME"),
+        "video_ids": [os.getenv("VIDEO_ID_1")],
+        "api_key": os.getenv("KEY_API"),
+    },
     # provide_context=True,
     dag=dag,
 )
+
+# default_args = {
+#     "owner": "airflow",
+#     "depends_on_past": False,
+#     "start_date": datetime(2024, 4, 11),
+#     "email_on_failure": False,
+#     "email_on_retry": False,
+#     "retries": 1,
+#     "retry_delay": timedelta(minutes=5),
+# }
+
+# dag = DAG(
+#     'youtube_comment_analysis',
+#     default_args=default_args,
+#     description='Analyze YouTube comments using Ollama',
+#     schedule_interval='@daily',
+#     catchup=False,
+# )
+
+# analyze_comments = PythonOperator(
+#     task_id='analyze_youtube_comments',
+#     python_callable=test,
+#     # provide_context=True,
+#     dag=dag,
+# )
 
 # if __name__ == "__main__":
 #     test()
